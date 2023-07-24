396e4d0631683d423b8ceb3019d18c55986d48a2
Leyla <leylacayli@hotmail.com>
2023-07-24 22:03:07 +0300

    US01_V2

--- src/test/java/tests/US01/TC01_AramaKutusuTesti.java
+++ src/test/java/tests/US01/TC01_AramaKutusuTesti.java
@@ -1,5 +1,6 @@
 package tests.US01;
 
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import pages.Page;
 import utilities.ConfigReader;
@@ -10,6 +11,8 @@ public class TC01_AramaKutusuTesti {
     public void test01() {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         Page page =new Page();
+        page.aramakutusu.sendKeys("iphone", Keys.ENTER);
+        Driver.closeDriver();
 
     }
 }
